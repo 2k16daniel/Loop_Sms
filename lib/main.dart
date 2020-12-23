da5e28ca37c0d15a3f9b1958776f96358b7be744
Daniel Teano <2k16daniel@gmail.com>
2020-12-23 13:07:00 +0800

    Added Number field with length

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,83 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp(MyApp());
+  runApp(MainPage());
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              WholeUI(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class WholeUI extends StatefulWidget {
+  WholeUI({Key key}) : super(key: key);
+
+  @override
+  _WholeUIState createState() => _WholeUIState();
+}
+
+class _WholeUIState extends State<WholeUI> {
+  final numberfield = TextEditingController();
+  final smsfield = TextEditingController();
+  @override
+  void dispose() {
+    numberfield.dispose();
+    smsfield.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: NumberField(),
+    );
+  }
+}
+
+class NumberField extends StatelessWidget {
+  const NumberField({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+          primaryColor: Colors.redAccent, primaryColorDark: Colors.red),
+      child: TextField(
+        keyboardType: TextInputType.phone,
+        maxLength: 11,
+        decoration: InputDecoration(
+          border:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.teal)),
+          hintText: 'input phone number here',
+          helperText: 'only accepts 11 digits phone number',
+          labelText: 'Phone number',
+          prefixIcon: Icon(
+            Icons.phone,
+            color: Colors.green[700],
+          ),
+        ),
+      ),
+    );
+  }
 }
